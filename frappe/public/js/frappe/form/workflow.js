af988076c5ad7ae4aa3e57b59ca9383cefa9aa1c
Vengadesh <m.vengadesh2019@gmail.com>
2025-05-21 23:27:25 +0530

    feat: Confirmation dialog when workflow action button is clicked

--- frappe/public/js/frappe/form/workflow.js
+++ frappe/public/js/frappe/form/workflow.js
@@ -102,25 +102,34 @@ frappe.ui.form.States = class FormStates {
 				if (frappe.user_roles.includes(d.allowed) && has_approval_access(d)) {
 					added = true;
 					me.frm.page.add_action_item(__(d.action), function () {
+						frappe.confirm(
+							__("Are you sure you want to take this action?"),
+							function () {
+								frappe.dom.freeze();
+								me.frm.selected_workflow_action = d.action;
+								me.frm.script_manager
+									.trigger("before_workflow_action")
+									.then(() => {
+										frappe
+											.xcall("frappe.model.workflow.apply_workflow", {
+												doc: me.frm.doc,
+												action: d.action,
+											})
+											.then((doc) => {
+												frappe.model.sync(doc);
+												me.frm.refresh();
+												me.frm.selected_workflow_action = null;
+												me.frm.script_manager.trigger(
+													"after_workflow_action"
+												);
+											})
+											.finally(() => {
+												frappe.dom.unfreeze();
+											});
+									});
+							}
+						);
 						// set the workflow_action for use in form scripts
-						frappe.dom.freeze();
-						me.frm.selected_workflow_action = d.action;
-						me.frm.script_manager.trigger("before_workflow_action").then(() => {
-							frappe
-								.xcall("frappe.model.workflow.apply_workflow", {
-									doc: me.frm.doc,
-									action: d.action,
-								})
-								.then((doc) => {
-									frappe.model.sync(doc);
-									me.frm.refresh();
-									me.frm.selected_workflow_action = null;
-									me.frm.script_manager.trigger("after_workflow_action");
-								})
-								.finally(() => {
-									frappe.dom.unfreeze();
-								});
-						});
 					});
 				}
 			});
