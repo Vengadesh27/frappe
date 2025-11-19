018e4d93408eb2e88ef6180a6510c0ce0c72874f
Vengadesh <m.vengadesh2019@gmail.com>
2025-11-19 22:11:34 +0530

    feat: enhance ControlInt to support expressions and short-hand amounts (2.5L, 1Cr)

--- frappe/public/js/frappe/form/controls/int.js
+++ frappe/public/js/frappe/form/controls/int.js
@@ -1,9 +1,11 @@
 frappe.ui.form.ControlInt = class ControlInt extends frappe.ui.form.ControlData {
 	static trigger_change_on_input_event = false;
 	static input_mode = "numeric";
+
 	make() {
 		super.make();
 	}
+
 	make_input() {
 		super.make_input();
 		this.$input.on("focus", () => {
@@ -11,34 +13,61 @@ frappe.ui.form.ControlInt = class ControlInt extends frappe.ui.form.ControlData 
 			return false;
 		});
 	}
-	validate(value) {
-		return this.parse(value);
+	parse_short_amount(value) {
+		if (!value) return value;
+		value = value.toUpperCase().trim();
+
+		const multipliers = {
+			K: 1000,
+			L: 100000,
+			LAC: 100000,
+			LAKH: 100000,
+			CR: 10000000,
+			CRORE: 10000000,
+			M: 1000000,
+			B: 1000000000,
+		};
+
+		const match = value.match(/^([0-9]*\.?[0-9]+)\s*([A-Z]+)$/);
+
+		if (!match) return value;
+
+		const number = parseFloat(match[1]);
+		const suffix = match[2];
+
+		let multiplier = multipliers[suffix] || 1;
+		return number * multiplier;
 	}
 	eval_expression(value) {
 		if (typeof value === "string") {
+			let short_val = this.parse_short_amount(value);
+			if (typeof short_val === "number") {
+				return short_val;
+			}
+
 			const parsed_components = value.match(/[^\d.,]+|[\d.,]+/g);
-			var parsed_value = value;
+			let parsed_value = value;
+
 			if (parsed_components !== null) {
 				parsed_value = parsed_components
-					.map((v) => {
-						return isNaN(parseFloat(v)) ? v : flt(v);
-					})
+					.map((v) => (isNaN(parseFloat(v)) ? v : flt(v)))
 					.join("");
 			}
+
 			if (parsed_value.match(/^[0-9+\-/*.() ]+$/)) {
-				// If it is a string containing operators
 				try {
 					return eval(parsed_value);
 				} catch (e) {
-					// bad expression
 					return value;
 				}
 			}
 		}
+
 		return value;
 	}
 	parse(value) {
-		return cint(this.eval_expression(value), null);
+		let val = this.eval_expression(value);
+		return cint(val, null);
 	}
 };
 
